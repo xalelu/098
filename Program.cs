3e16a526cf6ea7c36646b5493d85be6eceb9489f
xale <xale2018@gmail.com>
2022-07-23 14:58:42 +0800

    update xam

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace TestSelf
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! xam");
         }
     }
 }
