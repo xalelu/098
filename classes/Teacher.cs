926f7c0dce3dad303704f791ef468521bc786172
xale <xale2018@gmail.com>
2022-07-23 17:56:42 +0800

    add Develop

---
+++ classes/Teacher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestSelf.classes
+{
+    class Teacher
+    {
+        public string name;
+        public Dictionary<int, Student> sts;
+        public Teacher(string name)
+        {
+            this.name = name;
+        }
+        public void addStudent(Student st)
+        {
+            sts.Add(sts.Count, st);
+        }
+        public void removeStudent(int num)
+        {
+            sts.Remove(num);
+        }
+    }
+}
