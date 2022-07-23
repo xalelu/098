426f9861fb162b3b9cb88d017350ec9377df3ace
xale <xale2018@gmail.com>
2022-07-23 21:13:18 +0800

    Add Android class

---
+++ classes/Other/Android.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestSelf.classes.Other
+{
+    class Android
+    {
+        public Android()
+        {
+
+        }
+    }
+}
